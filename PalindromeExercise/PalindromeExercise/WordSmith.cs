199fa5538d2a321c31f94273d17e1dc1debba0e6
Adam Jankowski <ghost17814@gmail.com>
2023-05-03 14:55:27 -0400

    racecar

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool RaceCar(string palindrome)
+        {
+
+            int left = 0;
+            int right = palindrome.Length - 1;
+
+            while (left < right)
+            {
+                if (palindrome[left] != palindrome[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,29 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("ufotofu" , true)]
+        [InlineData("nolemonnomelon" , true)]
+        [InlineData("dogeeseseegod", true)]
+        [InlineData("lookmanohands", false)]
+        [InlineData("oozyratinasanitaryzoo", true)]
+        [InlineData("birdrib", true)]
+        [InlineData("dennissinned", true)]
+        [InlineData("amicodingyet", false)]
+        [InlineData("dontnod", true)]
+        [InlineData("toohottohoot", true)]
+        [InlineData("madamimadam", true)]
+        [InlineData("drabasafoolaloofasabard", true)]
+        public void Test1(string palindrome, bool expected )
         {
+            //ARRANGE
+            var test = new WordSmith();
 
+            //ACT
+            var actual = test.RaceCar(palindrome);
+
+            //ASSERT
+            Assert.Equal( expected, actual );
         }
     }
 }
